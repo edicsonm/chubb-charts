9a92b99aa3f3a4a360b57ce20d59460d9b95e630
Edicson Morales <edicsonm@gmail.com>
2019-03-12 15:35:06 +1100

    Generating new distribution at

---
+++ generate.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+var currentDate = date +"%T"
+helm package chubb-*-configmap
+helm repo index ./ --url https://edicsonm.github.io/chubb-charts/
+git add .
+git commit -m "Generating new distribution at $currentDate"
+git push origen master
