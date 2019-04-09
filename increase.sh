5d5621e13ea10f9f67ba3f9529cd5fb0161a5aca
Edicson Morales <edicsonm@gmail.com>
2019-04-09 19:37:22 +1000

    Saving changes at 19:37:22

--- increase.sh
+++ increase.sh
@@ -2,5 +2,5 @@
 
 versionLine=$(grep 'version' chubb-app/Chart.yaml)
 versionLine=$(eval $(echo $versionLine | sed -E 's/^(version:)(.*)(\.)([0-9]+)/echo "\1\2\3$((\4+1))"/g'))
-command="sed -i -e 's/^version:.*/$versionLine/' chubb-app/Chart.yaml"
+command="sed  's/^version:.*/$versionLine/' chubb-app/Chart.yaml"
 eval $command
