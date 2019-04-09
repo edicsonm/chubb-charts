6a3e69123ae2be11aadb0d148a55fdd7a19a5d1c
Edicson Morales <edicsonm@gmail.com>
2019-04-09 19:37:09 +1000

    Saving changes at 19:37:09

--- increase.sh
+++ increase.sh
@@ -1,23 +1,6 @@
 #!/usr/bin/env bash
 
-#line=$(sed -n '/^version/p' chubb-app/Chart.yaml)
-#echo "My line: "$line
-#grep 'version' chubb-app/Chart.yaml
-
-containerLine=$(grep 'version' chubb-app/Chart.yaml)
-echo $containerLine
-
-eval $(echo "version:0.1.34" | sed -E 's/(.*)(version:)(.*)([0-9]+)/echo "\1\2\3$((\4+1))"/g')
-
-
-
-
-#echo "version: 0.1.34" | sed -n 's/version: (.*)/1/p'
-
-#sed -r 's/(.*)(\?version=)([0-9]+)(.*)/echo "\1\2$((\3+1))\4"/ge' chubb-app/Chart.yaml
-
-#eval $(echo "barbaz?cache_version=1.4fooooo" | sed -E 's/(.*)(\?cache_version=)(.*)([0-9]+)/echo "\1\2\3$((\4+1))"/g')
-
-
-#string="CBX-1234--CBX-5678"
-#shortenedString=$(echo "$string" | sed -E "s/^([A-Za-z]+-[0-9]+)/\1/")
+versionLine=$(grep 'version' chubb-app/Chart.yaml)
+versionLine=$(eval $(echo $versionLine | sed -E 's/^(version:)(.*)(\.)([0-9]+)/echo "\1\2\3$((\4+1))"/g'))
+command="sed -i -e 's/^version:.*/$versionLine/' chubb-app/Chart.yaml"
+eval $command
